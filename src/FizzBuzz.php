f2dae6b5c7f0da1146d6125a6b57ac0468ee4754
Laura Bassani <bassanilaurarita@gmail.com>
2020-10-21 16:46:37 +0200

    Add class,function and test for Fizz and Buzz

---
+++ src/FizzBuzz.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App;
+
+
+
+class FizzBuzz {
+
+	public function changeValue($value) {
+        if($value == 3){
+            return 'Fizz';	
+
+        }
+        return 'Buzz';
+    }
+    
+    
+}
--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -3,15 +3,25 @@
 namespace Tests;
 
 use  PHPUnit\Framework\TestCase;
+use App\FizzBuzz;
 
 
-class ExampleTest extends TestCase {
+class FizzBuzzTest extends TestCase {
 
-	public function test_example(
+	public function test_return_Fizz_if_value_multiple_3(
 	) {
+		$fizzbuzz = new FizzBuzz();
+		$result = $fizzbuzz->changeValue(3);
 
-		$this->assertEquals(4, 4);
+		$this->assertEquals('Fizz', $result);
 	}
+	public function test_return_Fizz_if_value_multiple_5(
+		) {
+			$fizzbuzz = new FizzBuzz();
+			$result = $fizzbuzz->changeValue(5);
+	
+			$this->assertEquals('Buzz', $result);
+		}
 
 
 }
